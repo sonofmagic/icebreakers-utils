ffd6e095ce686e0c1336fa52226d91a989ddb38a
SonOfMagic <qq1324318532@gmail.com>
2022-07-12 15:25:56 +0800

    feat: add vue pdf hook

--- apps/wwwroot/src/App.vue
+++ apps/wwwroot/src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <div id="app">
-    <Pdf src="demo.pdf"></Pdf>
+    <canvas ref="element" v-bind="canvasAttributes" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import Pdf from '@icebreakers/vue-pdf'
+import { shallowRef, ref } from 'vue'
+import { usePdf } from '@icebreakers/vue-use-pdf'
+const element = shallowRef<HTMLCanvasElement>()
+const file = ref('demo.pdf')
+
+const {
+  rotateCW,
+  rotateCCW,
+  scaleIn,
+  scaleOut,
+  fitAuto,
+  fitWidth,
+  nextPage,
+  prevPage,
+  canvasAttributes
+} = usePdf({
+  file,
+  element,
+  onPageRenderSuccess: (p) => console.log('[PAGE_RENDER_SUCCESS]', p._pageInfo)
+})
 </script>
 
 <style lang="scss">
