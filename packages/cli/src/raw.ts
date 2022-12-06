84681a82a377fc163d9f1bf9406a49739e483c2f
sonofmagic <1324318532@qq.com>
2022-12-06 20:30:58 +0800

    feat: commit cli parallel version

--- packages/cli/src/raw.ts
+++ packages/cli/src/raw.ts
@@ -1,18 +1,20 @@
 import execa from 'execa'
 import { isExist, eachDir, currentDir } from './utils'
 import { PkgManager, lockFileEntries } from './enum'
-
+import PQueue from 'p-queue'
+import os from 'os'
 const createExecFile = (pkgM: PkgManager, command?: string): string => {
   return `${pkgM} ${command}`
 }
 
-export function raw (
+export function raw(
   pathLike: string,
   getCommand: string | ((pkgM: PkgManager) => string),
   subDir: boolean = false
 ) {
   const fn = subDir ? eachDir : currentDir
   return fn(pathLike, async (p) => {
+    const queue = new PQueue({ concurrency: os.cpus().length })
     for (let index = 0; index < lockFileEntries.length; index++) {
       const [pkgM, lockFile] = lockFileEntries[index]
       if (await isExist(lockFile)) {
@@ -20,13 +22,17 @@ export function raw (
           typeof getCommand === 'string' ? getCommand : getCommand(pkgM)
         const subprocess = execa(createExecFile(pkgM, command))
         subprocess.stdout?.pipe(process.stdout)
-        try {
-          await subprocess
-        } catch (error) {
-          console.error(error)
-        }
-        break
+        // 并发
+        queue.add(() => subprocess)
+        // 串行
+        // try {
+        //   await subprocess
+        // } catch (error) {
+        //   console.error(error)
+        // }
+        // break
       }
     }
+    await queue.onIdle()
   })
 }
