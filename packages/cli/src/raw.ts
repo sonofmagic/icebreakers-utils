4a7dd6420c90e88517d20027a77794659a634333
ice breaker <1324318532@qq.com>
2023-03-17 01:25:58 +0800

    release: @icebreakers/cli 0.1.1

--- packages/cli/src/raw.ts
+++ packages/cli/src/raw.ts
@@ -2,25 +2,26 @@ import execa from 'execa'
 import { isExist, eachDir, currentDir } from './utils'
 import { PkgManager, lockFileEntries } from './enum'
 import PQueue from 'p-queue'
-import os from 'os'
-const createExecFile = (pkgM: PkgManager, command?: string): string => {
-  return `${pkgM} ${command}`
-}
+// import os from 'os'
+// const createExecFile = (pkgM: PkgManager, command?: string): string => {
+//   return `${pkgM} ${command}`
+// }
 
-export function raw(
+export function raw (
   pathLike: string,
   getCommand: string | ((pkgM: PkgManager) => string),
   subDir: boolean = false
 ) {
   const fn = subDir ? eachDir : currentDir
   return fn(pathLike, async (p) => {
-    const queue = new PQueue({ concurrency: os.cpus().length })
+    // os.cpus().length
+    const queue = new PQueue({ concurrency: 1 })
     for (let index = 0; index < lockFileEntries.length; index++) {
       const [pkgM, lockFile] = lockFileEntries[index]
       if (await isExist(lockFile)) {
         const command =
           typeof getCommand === 'string' ? getCommand : getCommand(pkgM)
-        const subprocess = execa(createExecFile(pkgM, command))
+        const subprocess = execa(pkgM, command.split(/\s/))
         subprocess.stdout?.pipe(process.stdout)
         // 并发
         queue.add(() => subprocess)
