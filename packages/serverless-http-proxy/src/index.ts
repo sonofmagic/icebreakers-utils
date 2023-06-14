99cd5446ceee0578fd6984cc7b0dfe6e7cad76e7
ice breaker <1324318532@qq.com>
2023-06-14 20:55:27 +0800

    chore: @icebreakers/serverless-http-proxy@0.0.2

--- packages/serverless-http-proxy/src/index.ts
+++ packages/serverless-http-proxy/src/index.ts
@@ -1,13 +1,13 @@
-import express from 'express'
+import express, { type Express } from 'express'
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import cors from 'cors'
 import type { Options } from 'http-proxy-middleware'
-
+// import type {} from 'express-serve-static-core'
 export type ServerlessHttpProxyOptions = Record<string, Options>
 
 export function createProxyExpressInstance(
   options: ServerlessHttpProxyOptions
-) {
+): Express {
   const app = express()
 
   app.use(
