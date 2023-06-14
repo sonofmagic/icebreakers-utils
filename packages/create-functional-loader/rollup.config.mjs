39c908bf2c05dc286391ea186d9376ba70cedd8e
ice breaker <1324318532@qq.com>
2023-06-14 20:58:31 +0800

    chore: create-functional-loader@0.0.5

--- packages/create-functional-loader/rollup.config.mjs
+++ packages/create-functional-loader/rollup.config.mjs
@@ -3,7 +3,7 @@ import pkg from './package.json' assert { type: 'json' }
 // const pkg = require('./package.json')
 const outputs = getDefaultOutputs(pkg)
 const cjsOutput = outputs.find((x) => x.format === 'cjs')
-// cjsOutput.exports = 'none'
+cjsOutput.exports = 'named'
 export default createRollupConfig({
   output: [...outputs]
 })
