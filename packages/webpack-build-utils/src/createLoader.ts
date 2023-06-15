84155e516f381f32c7087c0b5c8cff5624a04fc7
ice breaker <1324318532@qq.com>
2023-06-15 23:27:37 +0800

    chore: release webpack-build-utils 0.0.2

--- packages/webpack-build-utils/src/createLoader.ts
+++ packages/webpack-build-utils/src/createLoader.ts
@@ -1,6 +1,10 @@
 import type { LoaderDefinitionFunction } from 'webpack'
 import { createLoader } from 'create-functional-loader'
+import type { CompatLoaderItem } from 'create-functional-loader'
 
-export default function (processor: LoaderDefinitionFunction) {
-  return createLoader(processor)
+export default function (
+  processor: LoaderDefinitionFunction,
+  options?: CompatLoaderItem
+) {
+  return createLoader(processor, options)
 }
